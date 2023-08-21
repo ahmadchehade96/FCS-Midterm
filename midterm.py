0300c7c199082785c9b575a4505a02032ea53ce4
ahmaddsf2021 <73748317+ahmaddsf2021@users.noreply.github.com>
2023-08-21 17:25:52 +0300

    added two files

--- midterm.py
+++ midterm.py
@@ -12,7 +12,7 @@ def login():
             attempts += 1
     else:
         print("Too many failed attempts. Try again later.")
-
+#https://stackoverflow.com/questions/60610009/multiple-choice-menu-python#:~:text=choice%20%3D%20input%20%28%22%22%22%201.%20M
 def admin_menu():
 
     while True:
@@ -26,4 +26,22 @@ def admin_menu():
         print("7. Exit")
 
         choose = input("Enter your choice: ")
+        if choose == "1":
+            Display_Statistics(employees)
+        elif choose == "2":
+            Add_Employee(employees)
+        elif choose == "3":
+            display_all_employees(employees)
+        elif choose == "4":
+            Change_Salary(employees)
+        elif choose == "5":
+            Remove_Employee(employees)
+        elif choose == "6":
+            Raise_Salary(employees)
+        elif choose == "7":
+            save_employee_data(employees)
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid choice. Please select again.")
 login()
