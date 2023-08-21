94e545f2021259f35e100d3c6abe7c86e8832894
ahmaddsf2021 <73748317+ahmaddsf2021@users.noreply.github.com>
2023-08-21 17:17:36 +0300

    added two files

--- midterm.py
+++ midterm.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-
+#https://stackoverflow.com/questions/47202331/python-username-and-password-with-3-attempts
 def login():
     attempts = 0
     while attempts < 5:
@@ -13,4 +12,18 @@ def login():
             attempts += 1
     else:
         print("Too many failed attempts. Try again later.")
+
+def admin_menu():
+
+    while True:
+        print("Admin Menu")
+        print("1. Display Statistics")
+        print("2. Add an Employee")
+        print("3. Display all Employees")
+        print("4. Change Employee’s Salary")
+        print("5. Remove Employee")
+        print("6. Raise Employee’s Salary")
+        print("7. Exit")
+
+        choose = input("Enter your choice: ")
 login()
